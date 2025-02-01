557fa664e9f664fa7c8b785e78cdb00f1484e453
antkky <antiky222@gmail.com>
2025-02-01 12:47:24 -0800

    project setup

---
+++ build/CMakeFiles/CryptoScraper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CryptoScraper.dir/main.cpp.o"
+  "CMakeFiles/CryptoScraper.dir/main.cpp.o.d"
+  "CryptoScraper"
+  "CryptoScraper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CryptoScraper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
